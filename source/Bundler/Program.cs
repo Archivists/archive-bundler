d070b0288b4ef2d1e4b9c182e5d4db8f55549c15
Alexander Groß <agross@therightstuff.de>
2014-03-28 23:16:16 +0100

    Adding empty executable

---
+++ source/Bundler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Topshelf;
+
+namespace Bundler
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      HostFactory.Run(x =>
+      {
+        x.Service<Listener>(s =>
+        {
+          s.ConstructUsing(name => new Listener());
+          s.WhenStarted(l => l.Start());
+          s.WhenStopped(l => l.Stop());
+        });
+
+        x.RunAsLocalSystem();
+        x.StartAutomaticallyDelayed();
+
+        x.UseLog4Net("log4net.config");
+
+        x.SetDescription("Bundler packages audio archives");
+        x.SetDisplayName("Bundler");
+        x.SetServiceName("Bundler");
+      });
+    }
+  }
+
+  class Listener
+  {
+    public void Start()
+    {
+    }
+
+    public void Stop()
+    {
+    }
+  }
+}
