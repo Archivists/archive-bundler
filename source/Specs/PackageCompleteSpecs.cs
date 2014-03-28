f20709a95f6359037808a6434df44267082df27a
Alexander Groß <agross@therightstuff.de>
2014-03-28 18:04:29 +0100

    Actually checking the dobbin XML to determine if the job is complete for processing

--- source/Specs/PackageCompleteSpecs.cs
+++ source/Specs/PackageCompleteSpecs.cs
@@ -1,5 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Xml;
+using System.Xml.XPath;
 
 using NUnit.Framework;
 
@@ -32,6 +37,8 @@ namespace Specs
     [TestCase("inputs/missing meta.xml/PBC-ISB-001365-A", false)]
     [TestCase("inputs/missing mp3/PBC-ISB-001365-A", false)]
     [TestCase("inputs/files for another job/some job id", false)]
+    [TestCase("inputs/invalid dobbin.result.xml/PBC-ISB-001365-A", false)]
+    [TestCase("inputs/invalid meta.xml/PBC-ISB-001365-A", false)]
     [TestCase("path does not exists", false)]
     public void Should_check_files(string path, bool expected)
     {
@@ -52,10 +59,39 @@ namespace Specs
       var jobId = Path.GetFileName(path);
 
       var dobbinResults = Directory.GetFiles(path, jobId + "*.dobbin.result.xml", SearchOption.TopDirectoryOnly);
-      var metas = Directory.GetFiles(path, jobId + "*.meta.xml", SearchOption.TopDirectoryOnly);
-      var mp3s = Directory.GetFiles(path, jobId + "*.mp3", SearchOption.TopDirectoryOnly);
+      var audioFilesComplete = AudioFilesComplete(dobbinResults);
 
-      return dobbinResults.Any() && metas.Any() && mp3s.Any();
+      var metas = Directory.GetFiles(path, jobId + "*.meta.xml", SearchOption.TopDirectoryOnly);
+
+      return dobbinResults.Any() && metas.Any() && audioFilesComplete;
+    }
+
+    static bool AudioFilesComplete(IEnumerable<string> dobbinResults)
+    {
+      try
+      {
+        var audioFiles = AudioFiles(dobbinResults);
+        return audioFiles.All(File.Exists);
+      }
+      catch (XmlException)
+      {
+        return false;
+      }
+    }
+
+    static IEnumerable<string> AudioFiles(IEnumerable<string> dobbinResults)
+    {
+      foreach (var result in dobbinResults)
+      {
+        var file = new XPathDocument(result).CreateNavigator().SelectSingleNode("/Dobbin/Path/text()").Value;
+        
+        if (!Path.IsPathRooted(file))
+        {
+          file = Path.Combine(Path.GetDirectoryName(result), file);
+        }
+
+        yield return file;
+      }
     }
   }
 }
